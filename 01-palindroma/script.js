952773cd5e3a374f282c7985ee00b335ba0831d5
Giuseppe Mele <giuseppemele9410@gmail.com>
2025-12-05 15:32:46 +0100

    feat : esercizio palindroma completato

--- 01-palindroma/script.js
+++ 01-palindroma/script.js
@@ -1,2 +1,36 @@
 /*Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma * */
+
+//output a schermo 
+const mex = document.getElementById("text-console") ;  
+
+//input utente
+const user = prompt("Inserisci una parola :"); 
+
+//invoco la funzione 
+palindroma(user.toLowerCase()); 
+
+
+
+// Funzione palindroma 
+function palindroma (parola) {
+
+    let risultato =""; 
+    
+    //salvo la parola al contrario 
+    for ( let x = parola.length - 1; x >= 0 ; x--) {
+        risultato += parola[x]; 
+    }
+
+
+    if(risultato === parola) {
+       mex.textContent = `La parola ${parola} è palindroma `; 
+        console.log("palindroma");
+    }
+    else {
+        mex.textContent = `La parola ${parola} non è palindroma `;  
+        console.log("non palindroma");
+    }
+
+    return risultato ; 
+} 
